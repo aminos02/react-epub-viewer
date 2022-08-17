3425b6795e827e231f4a1812c0ce3f3318681707
aminosReact <a.mohammed@kyo-conseil.com>
2022-08-17 10:06:13 +0100

    remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,27 @@
 import ReactDOM from 'react-dom';
 import Reader from 'containers/Reader'
+import { EpubViewer } from 'react-epub-viewer';
+import { useEffect, useRef } from 'react';
+import { ViewerRef } from 'types'
+// const EPUB_URL = "/react-epub-viewer/files/test.epub";
+const EPUB_URL = "/react-epub-viewer/files/test2.epub";
+// const EPUB_URL = "/react-epub-viewer/files/test3.epub";
+// const EPUB_URL = "/react-epub-viewer/files/test4.epub";
+// const EPUB_URL = "/react-epub-viewer/files/test5.epub";
+// const EPUB_URL = "/react-epub-viewer/files/test6.epub";
 
-const EPUB_URL = "/react-epub-viewer/files/Alices Adventures in Wonderland.epub";
 
-ReactDOM.render(<Reader url={EPUB_URL} />,  document.getElementById('root'));
+const App=()=>{
+    const viewerRef = useRef(null);
+    return (
+        <EpubViewer 
+        url={EPUB_URL}
+		ref={viewerRef}
+			epubOptions={{
+							allowScriptedContent: true,
+						}}
+        />
+    )
+}
+
+ReactDOM.render(<App />,  document.getElementById('root'));
